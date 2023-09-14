cde4d674e63d9a16db51308e18dc0b83aaa95774
Armando Ocampo <fooseaov@gmail.com>
2023-09-13 21:30:16 -0600

    borrador clase 2

--- scripts/borrador_clase_dos.R
+++ scripts/borrador_clase_dos.R
@@ -1,1 +1,18 @@
 ### borrador clase 2
+
+# cuando la poblacion es grande como en los países se prefiere realizar uncenso
+# esto debido a lo qcono9mico y eficaz
+# la información recabada se puede generalizar
+# toma de una muestra de la poblacion
+# poblacion, todo el dataset
+# muestra es un csubconjunto de datos
+
+# un parametro de población es un calculo realizado en un conjunto de datos
+# de una poblacion
+
+# una muestra estadística se calcula a partir de la muestra
+
+# para las muestras se utiliza la funcion slice_sample() y sample() para vectores
+?sample
+# toma una muestra de un determinado tamaño de los elementos de x con
+# y sin reemplazo
