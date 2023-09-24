d7a92e6471ae9da6ebaf2eb73f07799c087f7bdd
Armando Ocampo <fooseaov@gmail.com>
2023-09-23 20:26:13 -0600

    borrador clase 3

---
+++ modelos_lineales/borrador_clase_3.R
@@ -0,0 +1,7 @@
+## borrador clase 3
+# modelo lineal generalizado
+# extraccion de coeficientes
+# prediccion con modelos lineales generalizados
+# regresion logistica
+# regresion multiple
+# regrtesion de poisson
