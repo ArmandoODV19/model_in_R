3e56333c2d31b0b2edca3f0f1e77a98f3df011fd
Armando Ocampo <fooseaov@gmail.com>
2023-09-30 22:44:26 -0600

    creando cadena

--- scripts/cadenas_markov_borrador.R
+++ scripts/cadenas_markov_borrador.R
@@ -5,3 +5,16 @@ install.packages('markovchain')
 # llamando libreria
 
 library(markovchain)
+
+# creando matriz de transicion
+
+P = matrix(c(0.2,0.3,0.5,0.4,0.2,0.4,0.1,0.6,0.3),
+           nrow = 3,byrow = TRUE)
+P
+
+# creando la matriz
+
+mc <- new('markovchain', transitionMatrix = P,
+          states = c('a', 'b', 'c'), name = 'Cadena1')
+
+str(mc)
