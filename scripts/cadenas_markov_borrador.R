f17e3e1511b25b42830941a29dcfb7172b166715
Armando Ocampo <fooseaov@gmail.com>
2023-09-30 23:07:43 -0600

    fin ejemplo 1

--- scripts/cadenas_markov_borrador.R
+++ scripts/cadenas_markov_borrador.R
@@ -17,4 +17,44 @@ P
 mc <- new('markovchain', transitionMatrix = P,
           states = c('a', 'b', 'c'), name = 'Cadena1')
 
-str(mc)
+str(mc) # lista de ragumentos
+
+summary(mc)
+
+plot(mc)
+
+
+recurrentClasses(mc) # clases recurrentes
+
+transientStates(mc) # identifica estados transitorios
+
+absorbingStates(mc)  # identifica estados absorbentes
+
+
+# analisis probabilistico
+
+transitionProbability(object = mc, t0 = 'a', t1 = 'c')
+mc[1,3]
+
+# analisis en n pasos
+
+n <- 6
+mc ^ 6
+
+# distribucion haci adelante
+
+X0 = c(0.2,0.3,0.5) # La distribucion de X en t = 0
+n = 6
+Xn = X0*(mc^n)
+
+Xn
+sum(Xn)  # siempre 1
+
+# distribucion estacionaria
+
+disteEst <- steadyStates(mc)
+disteEst
+
+
+M <- 1/disteEst
+M
