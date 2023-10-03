5920470227e5709424e3511528aba6deeaeb05c2
Armando Ocampo <fooseaov@gmail.com>
2023-10-02 18:48:00 -0600

    borrador time series

--- scripts/time_series_borrador.R
+++ scripts/time_series_borrador.R
@@ -1,1 +1,19 @@
 ### time series borrador
+
+# secuencia de datos en orden cronológico
+# ordenado de manera secuencial
+
+# para graficar una serie de tiempo se utiliza
+# la funcion plot()
+# el tiempo se coloca en el eje de las equis
+# las observaciones en el eje equis de menor a mayor
+
+print(time_series)
+plot(time_series)
+
+
+
+
+
+
+
